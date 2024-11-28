6ea76b1baffc25ebf438599b8cdc2d5520f52ebd
pikapoopowo <pikachu93916@gmail.com>
2024-11-28 15:45:12 +0800

    Fixed codes

--- main.py
+++ main.py
@@ -38,17 +38,17 @@ def get_db():
                 db.close()
 
 @app.get("/todos", response_model=list[TodoResponse])
-def read_todos(db: Session = Depens(get_db())):
+def read_todos(db: Session = Depens(get_db)):
     return db.Query(Todo).all()
 
 @app.get("/todos/{todo_id}", response_model = TodoResponse)
-def read_todo(todo_id: int, db: Session = Depands(get_db())):
+def read_todo(todo_id: int, db: Session = Depands(get_db)):
     db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
     if not db_todo:
          raise HTTPException(status_code=404, details="Todo not found")
     return{db_todo}
 @app.put("/todo/{todo_id}", response_model=TodoResponse)
-def update_todo(todo_id: int, todo: TodoCreate, db: Session = Depends(get_db()))
+def update_todo(todo_id: int, todo: TodoCreate, db: Session = Depends(get_db)):
     db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
     if not db_todo:
          raise HTTPException(status_code=404, details="Todo not found")
@@ -57,7 +57,7 @@ def update_todo(todo_id: int, todo: TodoCreate, db: Session = Depends(get_db()))
     db.commit()
     return{db_todo}
 @app.delete("/todo/{todo_id}")
-def update_todo(todo_id: int, db: Session = Depends(get_db()))
+def update_todo(todo_id: int, db: Session = Depends(get_db)):
     db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
     if not db_todo:
          raise HTTPException(status_code=404, details="Todo not found")
